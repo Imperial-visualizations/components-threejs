99b064ce17cc353d85990408d214af26383aaedc
Robert King <robertkng104@gmail.com>
2020-09-08 11:39:02 +0100

    resolved merge

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import * as components from "./components"
+import * as VueGL from "vue-gl"
+
+
 const ImpVisThree={
   install(Vue){
+    Object.keys(VueGL).forEach( name => { 
+	    Vue.component(name,VueGL[name]);
+    });
     for(const componentName in components){
       const component = components[componentName];
       Vue.component(component.name,component);
